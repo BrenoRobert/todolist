7b93ed19d67b661ca05d3144a4e705ee64de0f6b
Breno Robert de Oliveira Ribeiro <59066325+BrenoRobert@users.noreply.github.com>
2023-10-15 19:07:14 -0300

    Initial commit

---
+++ src/main/java/br/com/brenorobert/todolist/user/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.brenorobert.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name="tb_user")
+public class UserModel {
+
+  @Id
+  @GeneratedValue(generator = "uuid")
+  private UUID id;
+
+  @Column(unique = true)
+  private String username;
+  private String name;
+  private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
